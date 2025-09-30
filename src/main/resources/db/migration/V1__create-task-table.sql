e7b742070baf0374c801e6c53bc4ca42c1aba463
Pamela <pamelacantaruti@hotmail.com>
2025-09-29 22:03:18 -0300

    feat: add create table script sql

---
+++ src/main/resources/db/migration/V1__create-task-table.sql
@@ -0,0 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE task (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    completed BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
